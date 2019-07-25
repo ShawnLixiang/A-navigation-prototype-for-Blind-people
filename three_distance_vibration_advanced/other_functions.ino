9eab0cb5d8dd9561784cb99bd487db744c7abb44
mhamilt <matt.hamilton@ed.ac.uk>
2019-07-25 16:00:51 +0100

    refactor

--- three_distance_vibration_advanced/other_functions.ino
+++ three_distance_vibration_advanced/other_functions.ino
@@ -20,48 +20,15 @@ int proximityRead(int proximityTrigPin, int proximityEchoPin)
 //============================================================
 void checkReading(int motorPin, int distance, int difference)
 {
-  if (abs(difference) < 100)
+  if (abs(distanceDifference) < 100)
   {
-    Serial.print("sensor1:");
-    Serial.println(distance);
-    if (0 <= distance && distance < 50)
+    for (int i = 0; i < threshCount; ++i)
     {
-      analogWrite(motorPin, 255);
-      delay(500);
-    }
-    else if (50 <= distance && distance < 100)
-    {
-      analogWrite(motorPin, 219);
-      delay(500);
-    }
-    else if (100 <= distance && distance < 150)
-    {
-      analogWrite(motorPin, 183);
-      delay(500);
-    }
-    else if (150 <= distance && distance < 200)
-    {
-      analogWrite(motorPin, 147);
-      delay(500);
-    }
-    else if (200 <= distance && distance < 250)
-    {
-      analogWrite(motorPin, 111);
-      delay(500);
-    }
-    else if (250 <= distance && distance < 300)
-    {
-      analogWrite(motorPin, 75);
-      delay(500);
-    }
-    else if (300 <= distance && distance < 350)
-    {
-      analogWrite(motorPin, 39);
-      delay(500);
-    }
-    else
-    {
-      analogWrite(motorPin, 0);
+      if (distance1 < distanceThresh[i])
+      {
+        analogWrite(motorPin, intensityMap[threshCount - 1 - i]);
+        break;
+      }
     }
   }
 }
