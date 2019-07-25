35201144ae60c15f3e5cfcfbec3a60c4c7e1c0b0
mhamilt <matt.hamilton@ed.ac.uk>
2019-07-25 15:01:45 +0100

    refine distance to sound timing, remove extraneous for loop in decision tree

--- distance_sound/distance_sound.ino
+++ distance_sound/distance_sound.ino
@@ -6,10 +6,14 @@
    Who Made it: YOUR NAME, DATE
 */
 const int SPEAKER = 3;
+//============================================================
 const int sensorUpdateMillis = 100;
 unsigned long lastReadMillis = 0;
+unsigned long toneLengthMillis = 50;
+unsigned long toneRepeatMillis = 400;
 //============================================================
 int bassTab[] = {1911, 1702, 1516, 1431, 1275, 1136, 1012}; //bass 1~7
+int distanceThresh[] = {50, 100, 150, 200, 250, 300, 350};
 //============================================================
 // defines pins numbers
 const int trigPin = 10;
--- distance_sound/other_functions.ino
+++ distance_sound/other_functions.ino
@@ -4,62 +4,34 @@ void someSeriesOfDecisions()
   if (abs(distanceDifference) < 100)
   {
     Serial.println(distance1);
-    if (0 <= distance1 && distance1 < 50)
+    if (distance1 < 50)
     {
-      Serial.println("hi1");
-      for (int note_index = 0; note_index < 7; note_index++)
-      {
-        sound(6);
-      }
+      sound(6);
     }
-    if (50 <= distance1 && distance1 < 100)
+    else if (distance1 < 100)
     {
-      Serial.println("hi2");
-      for (int note_index = 0; note_index < 7; note_index++)
-      {
-        sound(5);
-      }
+      sound(5);
     }
-    if (100 <= distance1 && distance1 < 150)
+    else if (distance1 < 150)
     {
-      Serial.println("hi3");
-      for (int note_index = 0; note_index < 7; note_index++)
-      {
-        sound(4);
-      }
+      sound(4);
     }
-    if (150 <= distance1 && distance1 < 200)
+    else if (distance1 < 200)
     {
-      Serial.println("hi4");
-      for (int note_index = 0; note_index < 7; note_index++)
-      {
-        sound(3);
-      }
+      sound(3);
     }
-    if (200 <= distance1 && distance1 < 250)
+    else if ( distance1 < 250)
     {
-      Serial.println("hi5");
-      for (int note_index = 0; note_index < 7; note_index++)
-      {
-        sound(2);
-      }
+      sound(2);
     }
-    if (250 <= distance1 && distance1 < 300)
+    else if (distance1 < 300)
+    {   
+        sound(1);      
+    }
+    else
     {
-      Serial.println("hi6");
-      for (int note_index = 0; note_index < 7; note_index++)
-      {
-        sound(1);
-      }
+        sound(0);     
     }
-    if (300 <= distance1 && distance1 < 350)
-    {
-      Serial.println("hi7");
-      for (int note_index = 0; note_index < 7; note_index++) {
-        sound(1);
-      }
-    }
-    Serial.println("hi");
   }
 }
 
@@ -89,12 +61,14 @@ void pinInit()
 }
 
 void sound(uint8_t note_index)
-{  
+{
   while ((millis() - lastReadMillis) < sensorUpdateMillis)
   {
+
     digitalWrite(SPEAKER, HIGH);
     delayMicroseconds(bassTab[note_index]);
     digitalWrite(SPEAKER, LOW);
     delayMicroseconds(bassTab[note_index]);
+
   }
 }
